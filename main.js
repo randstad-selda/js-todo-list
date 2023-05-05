b9a53b643cffd7ea0e6fbcb14a1c2573eb6c136c
samuelemadrigali <samuele.madrigali@gmail.com>
2023-05-05 11:59:55 +0200

    Todo with json server

--- main.js
+++ main.js
@@ -2,28 +2,6 @@
     VARIABLES
 ---------------------*/
 const formElement = document.querySelector("#form");
-
-// const lists = ["Fare la spesa", "Guardare la Roma", "Portare fuori il cane"];
-// const lists = [
-//   {
-//     userId: 1,
-//     id: 1,
-//     title: "Fare la spesa",
-//     completed: false,
-//   },
-//   {
-//     userId: 1,
-//     id: 2,
-//     title: "Guardare la Roma",
-//     completed: false,
-//   },
-//   {
-//     userId: 1,
-//     id: 3,
-//     title: "Portare fuori il cane",
-//     completed: true,
-//   },
-// ];
 /*--------------------
     FUNCTIONS
 ---------------------*/
@@ -37,21 +15,30 @@ const renderList = function () {
       liElement.classList.add("done");
     }
     liElement.id = elm.id;
-    document.querySelector("#list").append(liElement);
+    document.querySelector("#list").prepend(liElement);
   });
+};
+
+const renderError = function (err) {
+  document.querySelector("#list").innerHTML = err;
 };
 
 /*--------------------
     INIT - Caricamento della pagina
 ---------------------*/
 let lists = [];
-fetch(`https://jsonplaceholder.typicode.com/todos`)
+fetch(`http://localhost:3000/todos`)
   .then(function (response) {
+    if (!response.ok) throw new Error("Something went wrong! 😥");
+
     return response.json();
   })
   .then(function (data) {
     lists = data;
     renderList();
+  })
+  .catch(function (err) {
+    renderError(err);
   });
 /*--------------------
     EVENTS
@@ -60,14 +47,31 @@ formElement.addEventListener("submit", function (e) {
   e.preventDefault();
   const inputValue = document.querySelector("#input-field").value;
   const newTodo = {
-    userId: 1,
-    id: 3,
     title: inputValue,
     completed: false,
   };
-  lists.unshift(newTodo);
-  renderList();
-  formElement.reset();
+
+  fetch(`http://localhost:3000/todos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTodo),
+  })
+    .then(function (response) {
+      if (!response.ok) throw new Error("Something went wrong! 😥");
+      return response.json();
+    })
+    .then(function (data) {
+      lists.push(data);
+      renderList();
+    })
+    .catch(function (err) {
+      renderError(err);
+    })
+    .finally(function () {
+      formElement.reset();
+    });
 });
 
 document.querySelector("#list").addEventListener("click", function (e) {
@@ -75,8 +79,20 @@ document.querySelector("#list").addEventListener("click", function (e) {
 
   while (i < lists.length) {
     if (lists[i].id == e.target.id) {
-      lists[i].completed = !lists[i].completed;
-      e.target.classList.toggle("done");
+      fetch(`http://localhost:3000/todos/${e.target.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ completed: !lists[i].completed }),
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          lists[i] = data;
+          e.target.classList.toggle("done");
+        });
       return;
     }
     i++;
